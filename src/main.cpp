0fc7f73997a8bdcc0e18795b06d4d26b3d005b3f
Matthew Setiawan <94852286+matthew-setiawan@users.noreply.github.com>
2023-03-01 10:55:41 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,24 @@
 #include <map>
 #include <bitset>
 #include <STM32FreeRTOS.h>
+#include <cmath>
 using namespace std;
+
 //Step Size
 volatile uint32_t currentStepSize;
-SemaphoreHandle_t keyValMutex;//accesses the mutex object. 
+volatile uint32_t knobCount;
+volatile uint32_t prevKnob;
 
-
-//Key Array making it a global variable 
+//Key String/Array
 volatile uint32_t keyVal;
+
+//Constants
+const long pibitshift16 = 205887; // define the value of pi
+const long incrbitshift16 = 3;
+SemaphoreHandle_t keyArrayMutex;
+
+//Time
+int time = 0;
 
 //Key/Val Mapping
 std::map<std::string, std::uint32_t> keyvalmap = {{"111111111111",0},
@@ -29,48 +39,40 @@ std::map<std::string, std::uint32_t> keyvalmap = {{"111111111111",0},
                                                 {"111111111101",11},
                                                 {"111111111110",12}};
 
-
-std::map<std::string, std::string> keyvalmap2 = {{"111111111111","_"},
-                                                {"011111111111","C"},
-                                                {"101111111111","C#"},
-                                                {"110111111111","D"},
-                                                {"111011111111","D#"},
-                                                {"111101111111","E"},
-                                                {"111110111111","F"},
-                                                {"111111011111","F#"},
-                                                {"111111101111","G"},
-                                                {"111111110111","G#"},
-                                                {"111111111011","A"},
-                                                {"111111111101","A#"},
-                                                {"111111111110","B"}};
-
-
 const uint32_t stepSizes [] = {0,51076922,54112683,57330004,60740599,64274185,68178701,72231589,76528508,81077269,85899346,91006452,96418111};
+const uint32_t rawstepSizes [] = {0,262,277,294,311,329,349,369,392,415,440,466,494};
+
 //Constants
 const uint32_t interval = 100; //Display update interval
+
 //Pin definitions
 //Row select and enable
 const int RA0_PIN = D3;
 const int RA1_PIN = D6;
 const int RA2_PIN = D12;
 const int REN_PIN = A5;
+
 //Matrix input and output
 const int C0_PIN = A2;
 const int C1_PIN = D9;
 const int C2_PIN = A6;
 const int C3_PIN = D1;
 const int OUT_PIN = D11;
+
 //Audio analogue out
 const int OUTL_PIN = A4;
 const int OUTR_PIN = A3;
+
 //Joystick analogue in
 const int JOYY_PIN = A0;
 const int JOYX_PIN = A1;
+
 //Output multiplexer bits
 const int DEN_BIT = 3;
 const int DRST_BIT = 4;
 const int HKOW_BIT = 5;
 const int HKOE_BIT = 6;
+
 //Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
@@ -86,48 +88,50 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,LOW);
 }
 
-//convert integers to a binary string format for display
 const char* intToBinaryString(int value) {
-    static char buffer[13];  // Allocate a buffer to hold the binary string
-    std::bitset<12> bits((value & 0xFFF));  // Mask the last 12 bits and convert to binary format
+    static char buffer[15];  // Allocate a buffer to hold the binary string
+    std::bitset<14> bits((value & 0xFFF));  // Mask the last 12 bits and convert to binary format
     std::string binaryString = bits.to_string();  // Convert the binary format to a std::string
     binaryString.copy(buffer, sizeof(buffer));  // Copy the std::string to the buffer
     buffer[sizeof(buffer) - 1] = '\0';  // Add a null terminator to the end of the buffer
     return buffer;  // Return the binary string as a const char*
 }
 
-//reads columns and returns in byte format. 
 uint32_t readCols(){
   uint32_t retval = 0;
+
   //GET C-D#
   digitalWrite(RA0_PIN, LOW);
   digitalWrite(RA1_PIN, LOW);
   digitalWrite(RA2_PIN, LOW);
   digitalWrite(REN_PIN, HIGH);
+
   delayMicroseconds(3);
+
   retval += 2048*digitalRead(C0_PIN);
   retval += 1024*digitalRead(C1_PIN);
   retval += 512*digitalRead(C2_PIN);
   retval += 256*digitalRead(C3_PIN);
-  delayMicroseconds(10);
 
   //GET E-G
   digitalWrite(RA0_PIN, HIGH);
   digitalWrite(RA1_PIN, LOW);
   digitalWrite(RA2_PIN, LOW);
   digitalWrite(REN_PIN, HIGH);
+
   delayMicroseconds(3);
+
   retval += 128*digitalRead(C0_PIN);
   retval += 64*digitalRead(C1_PIN);
   retval += 32*digitalRead(C2_PIN);
   retval += 16*digitalRead(C3_PIN);
-  delayMicroseconds(10);
 
-  //GET G#-B
+  //GET C-D#
   digitalWrite(RA0_PIN, LOW);
   digitalWrite(RA1_PIN, HIGH);
   digitalWrite(RA2_PIN, LOW);
   digitalWrite(REN_PIN, HIGH);
+
   delayMicroseconds(3);
 
   retval += 8*digitalRead(C0_PIN);
@@ -136,94 +140,90 @@ uint32_t readCols(){
   retval += 1*digitalRead(C3_PIN);
 
   return retval;
-
 }
-int testvar = 0;
+
+u_int32_t readKnobs(){
+  uint32_t retval = 0;
+  //GET KNOB3
+  digitalWrite(RA0_PIN, HIGH);
+  digitalWrite(RA1_PIN, HIGH);
+  digitalWrite(RA2_PIN, LOW);
+  digitalWrite(REN_PIN, HIGH);
+  delayMicroseconds(3);
+  retval += 2*digitalRead(C0_PIN);
+  retval += 1*digitalRead(C1_PIN);
+
+  if((retval==1 && prevKnob==0)||(retval==2 && prevKnob==3)){
+    knobCount += 1;
+  }
+  else if((retval==0 && prevKnob==1)||(retval==3 && prevKnob==2)){
+    knobCount += -1;
+  }
+
+  return retval;
+}
+
+long testvar = 0;
 
 void sampleISR(){
   static uint32_t phaseAcc = 0;
   phaseAcc = phaseAcc + currentStepSize;
   uint32_t Vout = (phaseAcc>>24) - 128;
-  testvar = phaseAcc;
-  analogWrite(OUTR_PIN, Vout + 128);
+  analogWrite(OUTR_PIN, (Vout + 128)>>3);
+  time = time + incrbitshift16;
 }
 
 void scanKeysTask(void * pvParameters) {
-  //gets the time elapsed. 
-  // static uint32_t next = millis();
-  // static uint32_t count = 0;
-  // cout<<"scanning keys"<<endl;
-
   const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
   TickType_t xLastWakeTime = xTaskGetTickCount();
-  // cout<<"SCANNING"<<endl;
-  Serial.print("here");
-  
-  //run infinite loop
   while(1){
     vTaskDelayUntil( &xLastWakeTime, xFrequency );
-    xSemaphoreTake(keyValMutex,portMAX_DELAY);
-
-    keyVal = readCols();
-    if(readCols()==4095){
+    xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+    keyVal = readCols()+readKnobs();
+    xSemaphoreGive(keyArrayMutex);
+    if(readCols()%4096==4095){
         currentStepSize = stepSizes[0];
     }
     else{
       int indexs[12];
       int playedcount = 0;
-      for(int i = 0; i < 12; i++){
+      for(int i = 2; i < 14; i++){
         if(intToBinaryString(readCols())[i]=='0'){
-          currentStepSize = stepSizes[i+1];
+          currentStepSize = stepSizes[i-1];
           delayMicroseconds(20);
         }
       }
     }
-    //atomic storing
-    __atomic_store_n(&currentStepSize, currentStepSize, __ATOMIC_RELAXED);
-
-    xSemaphoreGive(keyValMutex);
-
-
-
   }
-
-
 }
-void displayUpdateTask(void * pvParameters) {
 
-  //run infinite loop
+void displayUpdateTask(void * pvParameters){
   const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
   TickType_t xLastWakeTime = xTaskGetTickCount();
-
-  while (1){
-
+  while(1){
     vTaskDelayUntil( &xLastWakeTime, xFrequency );
-
-    //get the semaphore value. 
-    // xSemaphoreTake(keyValMutex,portMAX_DELAY);
-
-
     u8g2.clearBuffer();         // clear the internal memory
     u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-    xSemaphoreTake(keyValMutex,portMAX_DELAY);//provide take to access a variable similar to threading lock.
-
+    //u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
+    xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
     u8g2.drawStr(2,20,intToBinaryString(keyVal));
     u8g2.drawStr(2,10,to_string(keyvalmap[intToBinaryString(keyVal)]).c_str());
+    xSemaphoreGive(keyArrayMutex);
     //currentStepSize = keystepmap[readCols()];
     u8g2.setCursor(2,20);
+    //u8g2.print(count++);
     u8g2.sendBuffer();          // transfer internal memory to the display
-    
     //Toggle LED
-    Serial.print(to_string(keyvalmap[intToBinaryString(keyVal)]).c_str());
+    cout << intToBinaryString(readCols()) << endl;
     digitalToggle(LED_BUILTIN);
-    xSemaphoreGive(keyValMutex);//breaking the threading lock
-
-
-
+    cout << testvar << endl;
+    cout << knobCount << endl;
   }
 }
+
 void setup() {
   // put your setup code here, to run once:
+
   //Set pin directions
   pinMode(RA0_PIN, OUTPUT);
   pinMode(RA1_PIN, OUTPUT);
@@ -233,32 +233,31 @@ void setup() {
   pinMode(OUTL_PIN, OUTPUT);
   pinMode(OUTR_PIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+
   pinMode(C0_PIN, INPUT);
   pinMode(C1_PIN, INPUT);
   pinMode(C2_PIN, INPUT);
   pinMode(C3_PIN, INPUT);
   pinMode(JOYX_PIN, INPUT);
   pinMode(JOYY_PIN, INPUT);
+
   //Initialise display
   setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
   delayMicroseconds(2);
   setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
   u8g2.begin();
   setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
+
   //Initialise UART
   Serial.begin(9600);
-  Serial.print("Hello World!");
-
+  Serial.println("Hello World");
 
   //Interrupt for sampleISR()
   TIM_TypeDef *Instance = TIM1;
-
-  //start a hardware timer to do polling. 
   HardwareTimer *sampleTimer = new HardwareTimer(Instance);
   sampleTimer->setOverflow(44000, HERTZ_FORMAT);
   sampleTimer->attachInterrupt(sampleISR);
   sampleTimer->resume();
-
 
   TaskHandle_t scanKeysHandle = NULL;
   xTaskCreate(
@@ -267,7 +266,7 @@ void setup() {
   64,      		/* Stack size in words, not bytes */
   NULL,			/* Parameter passed into the task */
   1,			/* Task priority */
-  &scanKeysHandle );  /* Pointer to store the task handle */
+  &scanKeysHandle);
 
   TaskHandle_t displayUpdateHandle = NULL;
   xTaskCreate(
@@ -278,17 +277,11 @@ void setup() {
   1,                     /* Task priority */
   &displayUpdateHandle);
 
-  keyValMutex = xSemaphoreCreateMutex();
+  //Knobs
+  keyArrayMutex = xSemaphoreCreateMutex();
 
   vTaskStartScheduler();
-
-
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-    //reads the key value
-
 }
